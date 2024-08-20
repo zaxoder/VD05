b38f9646e010cb2dc1c772a480e3af625a2c3c3e
zaxoder <167690364+zaxoder@users.noreply.github.com>
2024-08-21 00:18:37 +0300

    form

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+posts = []
 
 @app.route("/")
 def hello_world():
@@ -15,9 +16,19 @@ def hello():
 def hel():
     return render_template("contacts.html")
 
-@app.route("/stat/")
+@app.route("/stat/", methods=["GET", "POST"])
 def help():
-    return render_template("stat.html")
+    if request.method == 'POST':
+        title = request.form.get('title')
+        con = request.form.get('con')
+        cont = request.form.get('cont')
+        conten = request.form.get('conten')
+        content = request.form.get('content')
+
+        if title and content and con and cont and conten:
+            posts.append({'title': title, 'content': content, 'con': con, 'cont': cont, 'conten': conten})
+            return redirect(url_for('help'))
+    return render_template('stat.html', posts=posts)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
